54146c62fbd14249a2db5f3d8b7a21371c5eeb6d
Rafael Monteiro <diatosta123@gmail.com>
2018-04-12 09:45:10 +0300

    Fixed connection script.

--- PHP/Connect.php
+++ PHP/Connect.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER', '10.0.2.2'); // Temporary, ip address on the android emulator
+define('DB_SERVER', 'localhost'); // Temporary, ip address on the android emulator
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'gocar');
@@ -11,12 +11,13 @@ function openConnection(){
     // Create connection
     $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-    // Set connection to UTF-8 because weird characters and stuff
-    mysqli_query($conn, "SET NAME 'utf8'");
-
     // Check the connection
     if($conn == false){
-        dir("Connection failed: " . mysqli_connect_error());
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else{
+        // Set connection to UTF-8 because weird characters and stuff
+        mysqli_query($conn, "SET NAME 'utf8'");
     }
 
     return $conn;
